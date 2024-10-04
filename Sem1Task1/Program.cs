c37e5e277153055a73e414adefbeae53e9334cf4
Valery <wal_wali4@mail.ru>
2024-10-05 01:54:21 +0300

    Задача1

--- Sem1Task1/Program.cs
+++ Sem1Task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// a = 25, b = 5 => да
+// a = 2, b = 10 => нет
+// a = 9, b = -3 => да
+// a = -3, b = 9 => нет
+
+Console.Write("введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2 * num2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}

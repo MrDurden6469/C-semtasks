88f6284d134d222ba78ca4e44e189e839df6bbf5
Valery <wal_wali4@mail.ru>
2024-10-15 01:02:53 +0300

    Sem2Task2

---
+++ Sem2Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 1000 && num >= 100)
+{
+int SecondDigit = num / 10 % 10;
+int ThirdDigit = num % 10;
+int result = SecondDigit;
+int counter = 1;
+
+while (counter <= ThirdDigit)
+{
+    result = result * SecondDigit;
+    counter++;
+}
+
+Console.WriteLine(result);
+}
+else 
+{
+    Console.WriteLine("Некорректный ввод!");
+}

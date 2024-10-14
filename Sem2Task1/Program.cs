f12b7c35f3603ca6e61cf1b7ce789236874f9d14
Valery <wal_wali4@mail.ru>
2024-10-15 00:24:52 +0300

    Sem2Task1

---
+++ Sem2Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("введите трехзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ( num <= 999 && num >= 100 )
+
+{
+    int FirstDigit = num / 100;
+    int ThirdDigit = num % 10;
+
+    int result = FirstDigit * 10 + ThirdDigit;
+
+    Console.WriteLine(result);
+}
+else
+ {
+    Console.WriteLine("Некорректный ввод");
+ }

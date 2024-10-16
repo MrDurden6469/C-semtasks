b0a81120cbe059f0122a41a25936d6fad79f6036
Valery <wal_wali4@mail.ru>
2024-10-17 01:48:24 +0300

    Sem2Task4

---
+++ Sem2Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num <= -100 || num >= 100)
+{
+    while(num <= -999 || num > 999)
+        {
+            num = num / 10;
+        }
+
+    int result = num % 10;
+    Console.WriteLine("Третья цифра " + result);
+}
+
+else
+Console.WriteLine("Третьей цифры нет");

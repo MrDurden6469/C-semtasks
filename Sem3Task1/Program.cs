186204c171559d5e9e7c204b32ffd4536023e3a2
Valery <wal_wali4@mail.ru>
2024-10-21 01:16:56 +0300

    SemTask1

---
+++ Sem3Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//int [] array = new int[5] {3,6,7,8,9,};
+int[] array = {3, 7, 6, 8, 9};
+int num = 8;
+
+bool isExistNum = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == num)
+    {
+    isExistNum = true;
+    break;
+    }
+}
+//string result = isExistNum ? "Да" : "Нет";
+Console.Write(isExistNum ? "Да" : "Нет");
